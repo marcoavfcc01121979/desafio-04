d263b98bc6da9cadab36fde2378c4fe4c14f196e
Marco Ferreira <marcoavfcc@gmail.com>
2021-05-04 22:04:55 -0300

    update

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,50 @@
-import { Component } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import ReactModal from 'react-modal';
 
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  children: ReactNode;  
+}
+
+const Modal = ({ isOpen, setIsOpen, children }: ModalProps) => {
+  const [modalStatus, setModalStatus] = useState(isOpen)
+
+  useEffect(() => {
+    setModalStatus(isOpen)
+  },[isOpen])
+
+  return(
+    <ReactModal
+        shouldCloseOnOverlayClick={!false}
+        onRequestClose={setIsOpen}
+        isOpen={modalStatus}
+        ariaHideApp={false}
+        style={{
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            background: '#F0F0F5',
+            color: '#000000',
+            borderRadius: '8px',
+            width: '736px',
+            border: 'none',
+          },
+          overlay: {
+            backgroundColor: '#121214e6',
+          },
+        }}
+      >
+        {children}
+      </ReactModal>
+  )
+}
+
+/*
 class Modal extends Component {
   constructor(props) {
     super(props);
@@ -54,5 +98,5 @@ class Modal extends Component {
     );
   }
 };
-
+*/
 export default Modal;
